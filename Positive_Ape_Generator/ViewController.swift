219efe7cc930027fe9d506d025c66c8d08e2e588
Christine-horrocks <splogy2001@hotmail.com>
2018-06-01 16:56:30 +0100

    refactor

--- Positive_Ape_Generator/ViewController.swift
+++ Positive_Ape_Generator/ViewController.swift
@@ -15,14 +15,13 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var inspirationalMessage: UILabel!
     
+    let randomMessages = ["You can do it!", "Just stand up!", "I believe in you", "Reach around", "Go on", "That's it!", "Trust your feet", "Ooh, that is sketchy", "Flexy!", "Toes to tits", "Yes, that's it", "Chanel your inner ape", "Go Tom!", "You're the best", "Whoop Whoop", "Allez!", "Rock over", "Just a bit further....", "Grab the crack", "Pinch it!", "Nice guns ;-)", "OOOOOooooooooh", "Wow!", "Reach around", "Nearly there!", "You can do it!", "Just stand up", "Climb like a gibbon", "Be the rock", "Don't face plant", "Come on", "Just push harder", "You need more gusto", "Breathe!" ]
+    
+    let randomGifs = ["giphy", "gib_bridge", "gib_cat", "gib_hug", "chimp", "happy_orang", "hamock", "sloth", "baby_orang", "panda", "lizzard", "gib_walk", "gib_shake", "chimp_shove", "gor_clap", "chimp_butt", "chimp_fall"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        let randomGifs = ["giphy", "gib_bridge", "gib_cat", "orang_tig", "gib_hug"]
-//        let randomGif = Int(arc4random_uniform(UInt32(randomGifs.count)))
-//        let gif = UIImage.gif(name: randomGifs[randomGif])
-//
-//         happyApe.image = gif
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,13 +32,12 @@ class ViewController: UIViewController {
     
     @IBAction func shuffle(_ sender: Any) {
         
-        let randomMessages = ["You can do it!", "Just stand up!", "I believe in you", "Reach around", "Go on", "That's it!", "Trust your feet", "Ooh, that is sketchy", "Flexy!", "Toes to tits", "Yes, that's it", "Chanel your inner ape", "Go Tom!", "You're the best", "Whoop Whoop", "Allez!", "Rock over", "Just a bit further....", "Grab the crack", "Pinch it!", "Nice guns ;-)", "OOOOOooooooooh", "Wow!", "Reach around", "Nearly there!", "You can do it!", "Just stand up", "Climb like a gibbon", "Be the rock", "Remeber not to face plant", "Come on", "Can't you just push harder?", "You need more gusto" ]
         let randomMessage = Int(arc4random_uniform(UInt32(randomMessages.count)))
+        inspirationalMessage.text = randomMessages[randomMessage]
         
         let randomApe = arc4random_uniform(21)
         let image = UIImage(named: "ape\(randomApe)")
         
-        let randomGifs = ["giphy", "gib_bridge", "gib_cat", "gib_hug", "chimp", "happy_orang", "hamock", "sloth", "baby_orang", "panda", "lizzard", "gib_walk", "gib_shake", "chimp_shove", "gor_clap", "chimp_butt", "chimp_fall"]
         let randomGif = Int(arc4random_uniform(UInt32(randomGifs.count)))
         let gif = UIImage.gif(name: randomGifs[randomGif])
     
@@ -48,7 +46,6 @@ class ViewController: UIViewController {
         let medium = media[randomMedia]
         
         happyApe.image = medium
-        inspirationalMessage.text = randomMessages[randomMessage]
         
     }
     
